5322230f6d52487d0d9c8cefa7d56a150b585f6d
Tajammul Pasha <tpzuffari@gmail.com>
2016-09-29 19:38:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one.html', function (req, res) {
+app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
